76619bb02cc2e00f8fbdea38ed683ff7502ba041
OL1V1A <foreverdnmy@163.com>
2019-06-28 10:49:02 +0800

    commit common

---
+++ src/main/java/com/lwj/common/utils/HessianSerialization.java
@@ -0,0 +1,34 @@
+package com.lwj.common.utils;
+
+import com.caucho.hessian.io.Hessian2Input;
+import com.caucho.hessian.io.Hessian2Output;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ * @Auth: lwj
+ * @Date: 2019/6/25 13:29
+ */
+@Component
+public class HessianSerialization implements Serialization {
+    @Override
+    public byte[] serialize(Object obj) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Hessian2Output output = new Hessian2Output(byteArrayOutputStream);
+        output.writeObject(obj);
+        output.flush();
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    @Override
+    public <T> T deserialize(byte[] bytes, Class<T> clazz) throws IOException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        Hessian2Input input = new Hessian2Input(byteArrayInputStream);
+        return (T) input.readObject();
+    }
+
+
+}
